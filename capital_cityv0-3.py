fecf879e8b772fe33ccde2c3623625b987633ec3
JamesPC2005 <jamespconyt@gmail.com>
2025-07-22 22:08:34 -0700

    added more content, including a money tracker and end of turn functionality. game is now functional, just not fun yet.

--- capital_cityv0-3.py
+++ capital_cityv0-3.py
@@ -148,9 +148,11 @@ def game_loop(Board, Money):
     gameexit = False
     if Board == None:
         board = board_initialize()
+    else: board = Board
     money = Money
 
     while not gameexit:
+        earned = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -174,22 +176,55 @@ def game_loop(Board, Money):
                     else:
                         board[grid_y][grid_x] = "empty"
 
-
+        
             elif event.type == pygame.KEYDOWN:
-
+                
                 if event.key == pygame.K_SPACE: #reserved space for ending the turn
-                    print('space pressed')
+                    earned = turn(board, money)
+                    money += earned
 
                 if event.key == pygame.K_TAB: #start the store
                     store(board, money)
 
                 if event.key == pygame.K_ESCAPE: #reserved space for in game options menu
-                    print('esc key pressed')
+                    pygame.quit()
 
+        #################################################################################################################################################################
+        #################################################################################################################################################################
+        #################################################################End of Back end code, UI below##################################################################
+        #################################################################################################################################################################
+        #################################################################################################################################################################
+
+        # get mouse data
+        mouse = pygame.mouse.get_pos()
+        press = pygame.mouse.get_pressed()
+
+        # draw buttons hidden because the board is deawn after
+        #if display_width / 2 + 50 > mouse[0] > display_width / 2 - 50 and display_height / 4 + display_height / 12 + 50 > mouse[1] > display_height / 4 + display_height / 12:
+        #    pygame.draw.rect(gameDisplay, bright_green, (display_width / 2 - 50, display_height / 4 + display_height / 12, 100, 50))
+        #    pygame.draw.rect(gameDisplay, red, (display_width / 2 - 50, display_height / 4 + 2 * display_height / 12, 100, 50))
+        #    if press[0] == 1:
+        #        print("something")
+        
+        # draw Button labels
         gameDisplay.fill(grey)
         draw_board(board)
+        UIText = pygame.font.Font('freesansbold.ttf', 45)
+        Textsurface, Textrect = text_objects("Money: " + str(money), UIText)
+        Textrect.center = (display_width / 12, display_height / 16)
+        gameDisplay.blit(Textsurface, Textrect)
         pygame.display.update()
         clock.tick(60)
+
+def turn(board, Money):
+    earned = 0
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == "filled":
+                earned += 1
+
+    return earned
+
 
 # --- Start the game ---
 game_intro()
